f4d11577e11e8cb80fbc44ac607eef438e846d79
Mr-IngsS <54253110+Mr-IngsS@users.noreply.github.com>
2019-08-19 12:00:31 +0700

    BCT

---
+++ BCT.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+echo "=============================" | lolcat
+echo " Author : Mr.IngsS " | lolcat
+echo " My Team : BLACKCYBERTEAM " | lolcat
+echo " Massage : TIDAK ADA SISTEM YANG AMAN.. " | lolcat
+echo "=============================" | lolcat
+date
+echo " MENU... " | lolcat
+echo " ================== " | lolcat
+echo " [1]. LITESPAM" | lolcat
+echo " ================== " | lolcat
+echo " [2]. HACKFACEBOOK" | lolcat
+echo " ================== " | lolcat
+echo " [3]. NewsX" | lolcat
+echo " ================== " | lolcat
+echo " [4]. B-VIRUS Android" | lolcat
+echo " ================== " | lolcat
+echo " [5]. BIKIN DEFACE" | lolcat
+echo " ================== " | lolcat
+echo " [6]. SADAP kamera" | lolcat
+echo " ================== " | lolcat
+echo " [7]. SMS HACKING" | lolcat
+echo " ================== " | lolcat
+echo " [8]. BLACKCYBERTEAM" | lolcat
+echo " ================== | lolcat
+echo " [9]. Exiting" | lolcat
+echo " ======================== " | lolcat
+read -p "PILIH NO ===>" pill;
+
+case $pill in
+1)clear
+apt update && apt upgrade
+apt install php
+apt install git
+apt install toilet
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+
+;;
+
+2)clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install php
+pkg install git
+git clone https://github.com/bukanhacker/Sad.git
+cd Sad
+python2 Sad.py
+
+;;
+
+3)clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install php
+pkg install ruby
+gem install lolcat
+pkg install git
+git clone https://github.com/pengangguranmuda/NewsX.git
+cd NewsX
+sh newsx.sh
+
+;;
+
+4)clear
+pkg install python2
+pkg install php
+pkg install git
+git clone https://github.com/BlackCyberAnonim/B-VIRUS.git
+cd B-VIRUS
+python2 B-VIRUS.sh
+
+;;
+
+5)clear
+pkg install python2
+pkg install git
+git clone https://github.com/Mr-xDODOL/LITESCRIPT.git
+cd LITESCRIPT
+python2 LITESCRIPT.py
+
+;;
+
+6)clear
+git clone https://github.com/thelinuxchoise/saycheese.git
+cd saycheese
+bash saycheese.sh
+
+;;
+
+7)clear
+pkg install git python
+pkg install requests
+git clone https://github.com/N74NK/sms.git
+cd sms
+python sms.py
+
+;;
+
+8)clear
+echo " WE ARE LEGION " | lolcat
+echo "Peretasan Adalah Seni" | lolcat
+echo " ==========================>
+echo " Mr.IngsS " | lolcat
+echo " ÇØDÊX° "| lolcat
+echo " Mr.FidsS " | lolcat
+echo " Erlangga " | lolcat
+echo " ==========================>
+
+;;
+
+00)clear
+exit
+echo " TERIMA KASIH" | lolcat
+exit
+
+esac
+done
+done
